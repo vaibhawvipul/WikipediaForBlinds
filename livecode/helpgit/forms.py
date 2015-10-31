223b2293c7718178e2bef093d08082083d210f81
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2015-10-31 09:29:28 +0530

    Create forms.py

---
+++ livecode/helpgit/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from livecode.helpgit.models import UserProfile
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model=UserProfile
+        fields = ('Institution', 'City', 'Country')
