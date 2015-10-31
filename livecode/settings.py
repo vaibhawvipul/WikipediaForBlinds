dfadc1998ec7bb32bea483596dc211f1f35ebb38
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2015-10-31 09:22:34 +0530

    Update settings.py

--- livecode/settings.py
+++ livecode/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2376%uaduddxeej819wj4y8%8sc6xket@=l_c17*^ckz_qv&hl'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
