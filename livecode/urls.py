753a31a04a411f3591f10dab0ae31f9f0a7be31b
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2015-10-31 09:23:59 +0530

    Create urls.py

---
+++ livecode/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from livecode.helpgit import views
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'livecode.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'livecode.helpgit.views.user_login', name='user_login'),
+    url(r'^logout/', 'livecode.helpgit.views.user_logout', name='user_logout'),
+    url(r'^new/$', 'livecode.helpgit.views.new', name='new'),
+    url(r'^search/$', 'livecode.helpgit.views.search', name='search'),
+    url(r'^about/', 'livecode.helpgit.views.about', name='about'),
+    url(r'^register/', 'livecode.helpgit.views.register', name='register'),
+    url(r'^admin/', include(admin.site.urls)),
+)
